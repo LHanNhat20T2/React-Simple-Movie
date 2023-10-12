ebdfb2535734af4a0cc1ee1e46dc6cca2894a347
HanNhat02 <hannhat2468@gmail.com>
2023-10-12 11:36:01 +0700

    setup project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam,
+            amet a necessitatibus deleniti id cupiditate nobis rem similique
+            laborum sit voluptatem beatae eum error quisquam rerum assumenda
+            optio provident sint?
+        </div>
+    );
 }
 
 export default App;
