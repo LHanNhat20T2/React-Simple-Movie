129cd17a779bd95e7150b1cc4d0cf6c232355a7c
HanNhat02 <hannhat2468@gmail.com>
2023-10-12 18:50:51 +0700

    code movie-card

--- src/App.js
+++ src/App.js
@@ -3,35 +3,110 @@ import { NavLink } from "react-router-dom";
 function App() {
     return (
         <Fragment>
-            <header className="header flex items-center justify-center gap-x-5 text-white py-5 mb-10">
+            <header className="flex items-center justify-center py-5 mb-10 text-white header gap-x-5">
                 <span className="text-primary">Home</span>
                 <span>Movies</span>
             </header>
 
-            <section className="banner h-[400px] page-container">
-                <div className="w-full h-full rounded-lg relative">
+            <section className="banner h-[500px] page-container mb-20">
+                <div className="relative w-full h-full rounded-lg">
                     <div className="overlay absolute inset-0 bg-gradient-to-t rounded-lg from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)]"></div>
                     <img
                         src="https://d23.com/app/uploads/2019/04/1180w-600h_042419_avengers-endgame-need-to-know-780x440.jpg"
                         alt="Avengers"
-                        className="w-full h-full object-cover rounded-lg "
+                        className="object-cover w-full h-full rounded-lg "
                     />
-                    <div className="absolute left-5 bottom-5 w-full text-white">
-                        <h2 className="font-bold text-3xl mb-5 m">
+                    <div className="absolute w-full text-white left-5 bottom-5">
+                        <h2 className="mb-5 text-3xl font-bold m">
                             Avenger: End Game
                         </h2>
-                        <div className="flex items-center gap-x-3 mb-8">
-                            <span className="py-2 px-4 border rounded-md border-white border-solid ">
+                        <div className="flex items-center mb-8 gap-x-3">
+                            <span className="px-4 py-2 border border-white border-solid rounded-md ">
                                 Adventure
                             </span>
-                            <span className="py-2 px-4 border rounded-md border-white border-solid ">
+                            <span className="px-4 py-2 border border-white border-solid rounded-md ">
                                 Adventure
                             </span>
-                            <span className="py-2 px-4 border rounded-md border-white border-solid ">
+                            <span className="px-4 py-2 border border-white border-solid rounded-md ">
                                 Adventure
                             </span>
                         </div>
-                        <button className="py-3 px-6 rounded-lg bg-primary text-white font-medium">
+                        <button className="px-6 py-3 font-medium text-white rounded-lg bg-primary">
+                            Watch Now
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section className="mb-20 movies-layout page-container">
+                <h2 className="mb-10 text-3xl text-white capitalize">
+                    Now Playing
+                </h2>
+                <div className="grid grid-cols-4 gap-10 text-white movie-list">
+                    <div className="p-3 rounded-lg movie-card bg-slate-800">
+                        <img
+                            src="https://d23.com/app/uploads/2019/04/1180w-600h_042419_avengers-endgame-need-to-know-780x440.jpg"
+                            alt=""
+                            className="w-full h-[250px] object-cover rounded-lg mb-5"
+                        />
+                        <h3 className="mb-5 text-xl font-bold">
+                            Spiderman: HomeComing
+                        </h3>
+                        <div className="flex items-center justify-between mb-10 text-sm opacity-50">
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className="w-full px-6 py-3 capitalize rounded-lg bg-primary">
+                            Watch Now
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section className="mb-20 movies-layout page-container">
+                <h2 className="mb-10 text-3xl text-white capitalize">
+                    Top Rate
+                </h2>
+                <div className="grid grid-cols-4 gap-10 text-white movie-list">
+                    <div className="p-3 rounded-lg movie-card bg-slate-800">
+                        <img
+                            src="https://d23.com/app/uploads/2019/04/1180w-600h_042419_avengers-endgame-need-to-know-780x440.jpg"
+                            alt=""
+                            className="w-full h-[250px] object-cover rounded-lg mb-5"
+                        />
+                        <h3 className="mb-5 text-xl font-bold">
+                            Spiderman: HomeComing
+                        </h3>
+                        <div className="flex items-center justify-between mb-10 text-sm opacity-50">
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className="w-full px-6 py-3 capitalize rounded-lg bg-primary">
+                            Watch Now
+                        </button>
+                    </div>
+                </div>
+            </section>
+
+            <section className="mb-20 movies-layout page-container">
+                <h2 className="mb-10 text-3xl text-white capitalize">
+                    Trending
+                </h2>
+                <div className="grid grid-cols-4 gap-10 text-white movie-list">
+                    <div className="p-3 rounded-lg movie-card bg-slate-800">
+                        <img
+                            src="https://d23.com/app/uploads/2019/04/1180w-600h_042419_avengers-endgame-need-to-know-780x440.jpg"
+                            alt=""
+                            className="w-full h-[250px] object-cover rounded-lg mb-5"
+                        />
+                        <h3 className="mb-5 text-xl font-bold">
+                            Spiderman: HomeComing
+                        </h3>
+                        <div className="flex items-center justify-between mb-10 text-sm opacity-50">
+                            <span>2017</span>
+                            <span>7.4</span>
+                        </div>
+                        <button className="w-full px-6 py-3 capitalize rounded-lg bg-primary">
                             Watch Now
                         </button>
                     </div>
